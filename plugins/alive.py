a046b146e8115f6c01c91608ef28888695013495
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2023-05-01 20:13:30 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -30,7 +30,7 @@ async def _human_time_duration(seconds):
     
    
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(command("k") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://graph.org/file/11fb641b1724ec95a9420.jpg",
